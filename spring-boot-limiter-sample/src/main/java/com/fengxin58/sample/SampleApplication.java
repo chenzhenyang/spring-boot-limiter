c717d4ba4ceba8a32487d74a47ba6799df4fc981
chenzhenyang <czy_1@qq.com>
2019-07-31 10:27:29 +0800

    Squashed commit of the following:
    commit aed994e1986522f0036306deb930e919494476d2
    Author: chenzhenyang <czy_1@qq.com>
    Date:   Wed Jul 31 10:27:18 2019 +0800
    
    	1. oauth2 sample

--- spring-boot-limiter-sample/src/main/java/com/fengxin58/sample/SampleApplication.java
+++ spring-boot-limiter-sample/src/main/java/com/fengxin58/sample/SampleApplication.java
@@ -2,8 +2,12 @@ package com.fengxin58.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SampleApplication {
 
     public static void main(String[] args) {
